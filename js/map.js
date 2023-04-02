2c4fcc10c7d5e15b1c82f57f03753458c4aad167
Abdurrahman Hanif <awalstudiodesign@gmail.com>
2023-04-02 23:10:45 +0700

    update lokasi maps

--- js/map.js
+++ js/map.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
 		//Open street  Map
-		var coord = [40.738270, -74.008911]; // <--- coordinates here
+		var coord = [-6.113301, 106.910927]; // <--- coordinates here
 
 		var map = L.map('map-canvas', { scrollWheelZoom:false}).setView(coord, 19);
 
